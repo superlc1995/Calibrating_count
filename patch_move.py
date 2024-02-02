0eba3a69d04b79ef233e4a22d59acd3d2a074512
Li <lchen@harrier.cm.cluster>
2024-02-02 01:26:57 -0500

    fixing

--- patch_move.py
+++ patch_move.py
@@ -2,22 +2,22 @@ import numpy as np
 import shutil
 import os 
 
-labeled_list = np.loadtxt('label_list/sha_5.txt', dtype = str)
+labeled_list = np.loadtxt('label_list/sha-5.txt', dtype = str)
 all_croped = os.listdir('part_A/uncertain_data/')
 
 for file in all_croped:
     if (file.split('_')[0] + '_' + file.split('_')[1] + '.jpg') in labeled_list:
-        shutil.copyfile('uncertain_data/' + file, 'uncertain_data_5/' + file )
+        shutil.copyfile('part_A/uncertain_data/' + file, 'part_A/uncertain_data_5/' + file )
 
 
-labeled_list = np.loadtxt('label_list/sha_10.txt', dtype = str)
-
-for file in all_croped:
-    if (file.split('_')[0] + '_' + file.split('_')[1] + '.jpg') in labeled_list:
-        shutil.copyfile('uncertain_data/' + file, 'uncertain_data_10/' + file )
-
-labeled_list = np.loadtxt('label_list/sha_40.txt', dtype = str)
+labeled_list = np.loadtxt('label_list/sha-10.txt', dtype = str)
 
 for file in all_croped:
     if (file.split('_')[0] + '_' + file.split('_')[1] + '.jpg') in labeled_list:
-        shutil.copyfile('uncertain_data/' + file, 'uncertain_data_40/' + file )
+        shutil.copyfile('part_A/uncertain_data/' + file, 'part_A/uncertain_data_10/' + file )
+
+labeled_list = np.loadtxt('label_list/sha-40.txt', dtype = str)
+
+for file in all_croped:
+    if (file.split('_')[0] + '_' + file.split('_')[1] + '.jpg') in labeled_list:
+        shutil.copyfile('part_A/uncertain_data/' + file, 'part_A/uncertain_data_40/' + file )
